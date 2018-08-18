5571e3ea52d767ecc909d4f4831f4c369e8c73f9
Matt Salo <mattsalo@gmail.com>
2018-08-17 19:44:21 -0500

    updated setup script to use 777 permissions on subfolder instead of chown

--- blog/management/commands/opendbpermissions.py
+++ blog/management/commands/opendbpermissions.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
-import os, pwd, grp
+import os
+# import pwd, grp
 
 class Command(BaseCommand):
     help = "backs up the current sqllite db on an s3 bucket"
@@ -10,6 +11,7 @@ class Command(BaseCommand):
         sub_folder = os.path.dirname(db_file)
         # set file permissions
         os.chmod(db_file, 777)
-        uid = pwd.getpwnam("www-data")[2]
-        gid = grp.getgrnam("www-data")[2]
-        os.chown(sub_folder, uid, gid)
+        os.chmod(sub_folder, 777)
+        # uid = pwd.getpwnam("www-data")[2]
+        # gid = grp.getgrnam("www-data")[2]
+        # os.chown(sub_folder, uid, gid)
