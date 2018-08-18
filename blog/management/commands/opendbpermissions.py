9a53a19f7a41acfdbf7f8bc44aa74cb8506a3231
Matt Salo <mattsalo@gmail.com>
2018-08-17 19:58:44 -0500

    changed 777 to 0777 to pass in octal values (oops)

--- blog/management/commands/opendbpermissions.py
+++ blog/management/commands/opendbpermissions.py
@@ -10,8 +10,8 @@ class Command(BaseCommand):
         db_file = os.path.abspath(settings.DATABASES['default']['NAME'])
         sub_folder = os.path.dirname(db_file)
         # set file permissions
-        os.chmod(db_file, 777)
-        os.chmod(sub_folder, 777)
+        os.chmod(db_file, 0777)
+        os.chmod(sub_folder, 0777)
         # uid = pwd.getpwnam("www-data")[2]
         # gid = grp.getgrnam("www-data")[2]
         # os.chown(sub_folder, uid, gid)
