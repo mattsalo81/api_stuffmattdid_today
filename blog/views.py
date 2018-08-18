94369fd9b6e3bf4c185ab54481f63f5a79848ddb
Matt Salo <mattsalo@gmail.com>
2018-08-17 23:14:28 -0500

    added in a crappy website url that backsup the db will probably only use this for development

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.management import call_command
 
 # Create your views here.
+def backupdb_view(request):
+    """
+    backs up the current db in an S3 bucket
+    """
+    call_command('backupdb')
+    return HttpResponse("Database Backed up")
